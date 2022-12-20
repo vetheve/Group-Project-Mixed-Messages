60b444a9d2a2b5bc82efc1213872ab3a27092e2e
vetheve <victorien.etheve@outlook.fr>
2022-12-20 10:09:09 +0000

    ADD : Challenge Project: Find Your Hat

--- index.js
+++ index.js
@@ -1,24 +1,4 @@
 /**
- * Create a Local Server with Express and Opening a Website.
- */
-
-// Import the express and open modules
-import express from 'express';
-import open from 'open';
-
-// Create an instance of the express app
-const app = express();
-
-// Set up your routes and middleware here
-
-// Start the server on port 3000
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-  open(`http://localhost:${port}`);
-});
-
-/**
  * Update the text of the li elements for each zodiac sign.
  */
 
